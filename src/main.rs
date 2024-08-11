37ab38d3ef3c009d996fbaf327a41a4f67722bfd
doomguy <artiomat13100@gmail.com>
2024-08-12 02:21:33 +0300

    Got a droplet going!

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ use nix::{libc, unistd};
 
 use std::vec::Vec;
 use std::time::Duration;
-
 use std::io::{stdout, stdin, Write};
 use std::thread::{sleep};
+use std::cmp::{min, max};
+
+use rand::seq::SliceRandom;
+use rand;
+use rand::Rng;
 
 enum Color {
     Red,
@@ -15,6 +19,7 @@ enum Color {
     DarkBlue,*/
 }
 
+const TAIL_LEN : u16 = 5;
 
 const GLYPHS: [char; 8] = [
    'ぁ',
@@ -59,6 +64,11 @@ fn clear() {
     stdout().flush().expect("Flush");
 }
 
+fn glyph() -> char {
+    let mut rng = rand::thread_rng();
+    GLYPHS[rng.gen_range(0..GLYPHS.len())]
+}
+
 /// Set cursor, relative to top-left is [0,0]
 fn set_cur(x: u16, y: u16) {
    print!("\x1b[{};{}H", y+1, x+1);
@@ -70,6 +80,7 @@ fn main() {
     
     // Stores all the positions and stuff.
     let mut rain: Vec<[u16; 2]> = Vec::new();
+    rain.push([2,3]);
 
     loop {
         // If the size changes we reset the whole ordeal
@@ -80,9 +91,44 @@ fn main() {
         }
         
         clear();
-        set_cur(1,1);
-        put(GLYPHS[0], Color::Red);
-        sleep(Duration::new(0, 1_000_000 * 250));
+       
+        let mut do_pop = false;
+
+        // Render the drops and their tails & drop them
+        for drop in &mut rain {
+            if drop[1] as i32 - TAIL_LEN as i32 >= size[1] as i32 {
+                // We can pop() because this drop is GUARANTEED to be the lowest drop if it goes
+                // out of frame, that is because we generate them so that they are never on the
+                // same height!!!!
+                // XXX: Maybe just reset y????
+                do_pop = true;
+                continue;
+            }
+            
+            if drop[1] > 0 {
+                let y_top = max(0, (drop[1] as i32) - TAIL_LEN as i32) as u16;
+                for y in y_top..=drop[1] {
+                    set_cur(drop[0], y);
+                    // Head of tail is light green
+                    if y == drop[1] { 
+                        put(glyph(), Color::Green);
+                    } else {
+                        put(glyph(), Color::DarkGreen);
+                    }
+                }
+            }
+
+            drop[1] += 1;
+        }
+
+        if do_pop {
+            return;
+            rain.pop();
+        }
+
+        // set_cur(1,1);
+        // put(GLYPHS[0], Color::Red);
+        sleep(Duration::new(0, 1_000_000 * 50));
     }
 }
 
